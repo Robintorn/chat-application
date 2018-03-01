79b664df491e92baeeeb292f4a474eb2a2aeb420
robin torn <robin.torn.98@gmail.com>
2018-03-01 13:43:21 +0100

    Register and login almost working

--- src/database/FirebaseRepository.js
+++ src/database/FirebaseRepository.js
@@ -121,6 +121,7 @@ export default class {
             }
             else{
                 let reg = document.getElementById("registration/login").style.display = "block";
+                let welcome = document.getElementById("welcome").style.display = "none";
                 let logout = document.getElementById("logout").style.display = "none";  
                 console.log("Logged out");
             }
